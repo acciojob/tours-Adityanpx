f08889e926bff89684b4afee4ca0176048a7d35a
Aditya Gavali <adityagavali0911@gmail.com>
2025-05-20 15:36:45 +0530

    Done ✅

---
+++ src/components/Tours.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Tour from './Tour'
+
+function Tours({tours , removeTour}) {
+  return (
+    <div>
+        {tours.map((tour)=>(
+            <Tour key={tour.id} {...tour} removeTour={removeTour} />
+        ))};
+
+    </div>
+  )
+}
+
+export default Tours
